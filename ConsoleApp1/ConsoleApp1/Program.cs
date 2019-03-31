2a1541bf844e3716098df01f13aa6ba1ae3ecacb
Bagus eko <baguseko8814@gmail.com>
2019-03-31 14:11:10 +0700

    ConsoleApp1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek dari class Mahasiswa
+            mahasiswa saya = new mahasiswa();
+            mahasiswa kamu = new mahasiswa();
+
+            // mengeset nilai propertis objek saya dan kamu
+            saya.Nim = "18.11.2309";
+            saya.Nama = "Bagus";
+            saya.Ipk = 3.95f;
+
+            kamu.Nim = "18.11.2222";
+            kamu.Nama = "sopo";
+            kamu.Ipk = 3.70f;
+
+            // memanggil metho registrasi dan isi krs
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+
+        }
+    }
+}
